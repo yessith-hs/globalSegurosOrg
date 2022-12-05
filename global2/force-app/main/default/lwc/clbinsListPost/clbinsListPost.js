090d87937a924a56a68ae8e5d4e25ac4243b6550
Yessith Estiven Herrera Sanjuan <yessith@cloudblue.us>
2022-12-05 11:32:44 -0500

    Get data cms & btn nex content cms

--- global2/force-app/main/default/lwc/clbinsListPost/clbinsListPost.js
+++ global2/force-app/main/default/lwc/clbinsListPost/clbinsListPost.js
@@ -1,31 +1,26 @@
-import { LightningElement, api, wire} from "lwc";
-import getContentList from "@salesforce/apex/ManagedContentController.getContentList";
-import basePath from "@salesforce/community/basePath";
-import { formatDate } from "c/clbinsUtils";
+import { LightningElement, api, wire, track } from 'lwc'
+import getContentList from '@salesforce/apex/ManagedContentController.getContentList'
+import basePath from '@salesforce/community/basePath'
+import { formatDate } from 'c/clbinsUtils'
 export default class ClbinsListPost extends LightningElement {
-  @api topic;
-  posts;
-  maxEntries = 2;
-  hasEntries = true;
+  @api topic
+  hasEntries
+  listEntries
+  maxEntries = 10
+  @track maxEntriesShow = 2
 
-
+  // * Get Content List CMS
   @wire(getContentList, {
     page: 0,
-    pageSize: "$maxEntries",
-    language: "es",
-    filterby: "$topic"
+    pageSize: '$maxEntries',
+    language: 'es',
+    filterby: '$topic'
   })
   wiredContent({ data, error }) {
-    console.log(
-      "🚀 ~ file: clbinsListPost.js:18 ~ ClbinsListPost ~ hasEntries",
-      this.hasEntries
-    );
-
-
     if (data) {
-      this.posts = data.map((entry) => {
-        const { Title, Descripcion, Imagen } = entry.contentNodes;
-        const date = formatDate(entry.publishedDate);
+      this.listEntries = data.map(entry => {
+        const { Title, Descripcion, Imagen } = entry.contentNodes
+        const date = formatDate(entry.publishedDate)
 
         return {
           key: entry.contentKey,
@@ -34,24 +29,26 @@ export default class ClbinsListPost extends LightningElement {
           descripcion: Descripcion.value,
           imageUrl: `${basePath}/sfsites/c${Imagen.url}`,
           imageAltText: Imagen.value
-        };
-      });
+        }
+      })
 
-         console.log("original", data.length);
-    console.log("post", this.posts.length);
-
-      this.error = undefined;
+      this.error = undefined
     }
     if (error) {
-      console.log("Error: " + JSON.stringify(error));
+      console.log('Error: ' + JSON.stringify(error))
     }
   }
 
-  handleGetEntries() {
-    this.maxEntries += 2;
+  nextEntries() {
+    this.maxEntriesShow += 2
   }
 
-  renderedCallback() {
-    console.log("new render");
+  get entries() {
+    if (this.listEntries) {
+      const totalEntries = this.listEntries.length
+      this.hasEntries = this.maxEntriesShow >= totalEntries ? false : true
+
+      return this.listEntries.slice(0, this.maxEntriesShow)
+    }
   }
 }
