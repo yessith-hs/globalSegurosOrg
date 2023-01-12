5fd8a17525118eec6b0b89e952257798d72d320f
Yessith Estiven Herrera Sanjuan <yessith@cloudblue.us>
2023-01-12 17:03:32 -0500

    Add insurance LWC component

--- global2/force-app/main/default/lwc/clbinsCardInsurance/clbinsCardInsurance.js
+++ global2/force-app/main/default/lwc/clbinsCardInsurance/clbinsCardInsurance.js
@@ -1,18 +1,39 @@
-import { LightningElement,api } from 'lwc';
-import { NavigationMixin } from "lightning/navigation";
+import { LightningElement, api } from 'lwc'
+import { NavigationMixin } from 'lightning/navigation'
 
-import basePath from "@salesforce/community/basePath";
+import basePath from '@salesforce/community/basePath'
 export default class ClbinsCardInsurance extends NavigationMixin(
   LightningElement
-)  {
-   @api insurance;
+) {
+  @api insurance
 
-  handleClick(event) {
+  handleClick (event) {
     this[NavigationMixin.Navigate]({
       type: 'standard__webPage',
       attributes: {
         url: `${basePath}/gestiona-tus-seguros/detalle-del-seguro?insuranceId=${event.currentTarget.dataset.id}`
       }
     })
+  }
+
+  get getStyleRamoSeguro() {
+    return `
+    height: 12px;
+    width: 12px;
+    border-radius: 50%;
+    background-color: ${this.insurance.color};
+    display: block;
+    position: absolute;
+    top: 19px;
+    right: 16px;
+    `
+  }
+
+  get getStyleRamoSeguroBorder() {
+    return `
+    box-shadow:  0 2px 0 0 ${this.insurance.color};
+    margin-bottom: 0.7rem;
+    padding: 1rem 1rem 0;
+    `
   }
 }
