1cdbfd21120a3b834e775d1587db93005e13bd57
yessith herrera sanjuan <yessith@cloudblue.us>
2023-03-03 08:01:16 -0500

    change name apex class custom feed item

---
+++ global2/.sfdx/typings/lwc/apex/clb_ins_CustomFeedItem.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/clb_ins_CustomFeedItem.createNewFeedItem" {
+  export default function createNewFeedItem(param: {parentId: any, blogId: any, blogTitle: any, blogTopic: any, body: any, title: any, firstName: any, lastName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/clb_ins_CustomFeedItem.getFeedItems" {
+  export default function getFeedItems(param: {blogId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/clb_ins_CustomFeedItem.getFeedItem" {
+  export default function getFeedItem(param: {itemId: any}): Promise<any>;
+}
