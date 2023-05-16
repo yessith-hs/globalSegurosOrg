8e8b4c513d04fef5a5b96e9e5b9ff108c3d9148e
yessith herrera sanjuan <yessith@cloudblue.us>
2023-05-16 16:57:43 -0500

    add styles insurance detail card

--- global2/force-app/main/default/lwc/clbinsInsuranceDetailsContentCard/clbinsInsuranceDetailsContentCard.js
+++ global2/force-app/main/default/lwc/clbinsInsuranceDetailsContentCard/clbinsInsuranceDetailsContentCard.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class ClbinsInsuranceDetailsContentCard extends LightningElement {}
+export default class ClbinsInsuranceDetailsContentCard extends LightningElement {
+  @api insurance
+}
