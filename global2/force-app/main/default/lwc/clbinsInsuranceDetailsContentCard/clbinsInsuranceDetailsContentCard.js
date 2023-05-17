b2883e08d416e7d56ac45b1d1888302bbc208028
yessith herrera sanjuan <yessith@cloudblue.us>
2023-05-17 16:03:40 -0500

    fix styles insurance detail card

--- global2/force-app/main/default/lwc/clbinsInsuranceDetailsContentCard/clbinsInsuranceDetailsContentCard.js
+++ global2/force-app/main/default/lwc/clbinsInsuranceDetailsContentCard/clbinsInsuranceDetailsContentCard.js
@@ -1,5 +1,15 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api } from 'lwc'
 
 export default class ClbinsInsuranceDetailsContentCard extends LightningElement {
   @api insurance
+
+  get hasCoverages() {
+    const { coverages } = this.insurance
+    const hasCoverage = coverages.length === 0
+    return !hasCoverage
+  }
+
+  showCoverage() {
+    this.template.querySelector('.showcoverage').classList.toggle('active')
+  }
 }
